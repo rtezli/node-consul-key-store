fa8c88a59ee13c95a946d36826c6ae0669a5ad66
Robert Tezli <robert@pixills.com>
2016-06-29 12:50:36 +0200

    updated scripts

--- publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-image=$DOCKER_IMAGE_PREFIX$(pwd | awk '{n=split($1,a,"/"); print a[n]}')
-version=$(git describe --abbrev=0 --tags)
-
-docker push $DOCKER_REGISTRY/$image:$version
-docker push $DOCKER_REGISTRY/$image
